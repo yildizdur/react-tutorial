1ca8b1d641aa3bf8cd2622a3b28ff96430193c07
yildizdur <yildizdur@outlook.com>
2021-12-18 22:35:18 +0300

    Yınput component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import moment from 'moment';
 import 'moment/locale/tr';
+import YInput from './components/YInput';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <h1>Hello React.js !</h1>
       <h2>Tarih: </h2>
       <p> {moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
+      <YInput name="yildiz" />
     </div>
   );
 }
