9b0aeadb638d9d875659bbf607e877f92af119db
yildizdur <yildizdur@outlook.com>
2021-12-18 22:23:32 +0300

    2 files modified - moment added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './style.css';
+import moment from 'moment';
+import 'moment/locale/tr';
 
 export default function App() {
   return (
     <div>
       <h1>Hello React.js !</h1>
+      <h2>Tarih: </h2>
+      <p> {moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
     </div>
   );
 }
